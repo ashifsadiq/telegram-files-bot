a4f9d5781928df19f64c47f46d66d62af5f729d9
techcoblud <ashifsadiq@gmail.com>
2025-08-15 12:14:57 +0530

    remove Secret data

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-// https://api.telegram.org/bot6410770571:AAGKx0DGe_hfQ9ASSUJ88tPn0WJ5Ql0MWBk/setWebhook?url=https://cc491ba0f821.ngrok-free.app
-// /api/telegram/webhooks/inbound
 Route::prefix('telegram/webhooks')->group(function () {
     Route::post('inbound', [TelegramWebController::class, 'bot'])->name('api.webhook.inbound');
     Route::post('update', [TelegramWebController::class, 'store'])->name('api.webhook.update');
